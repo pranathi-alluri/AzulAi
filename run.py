2cbb1fff8cbfc872012870bd52e39f884de8bdf4
pranathi13 <alluri.p@northeastern.edu>
2023-04-26 22:00:05 -0400

    Fixed spelling issue

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ from player import mctsPlayer
 from utils import *
 
 
-players = [naive_Player.NaivePlayer(0), naive_player.NaivePlayer(1)]
+players = [naive_player.NaivePlayer(0), naive_player.NaivePlayer(1)]
 # ,naive_player.NaivePlayer(2), naive_player.NaivePlayer(3)]
 
 gr = GameRunner(players, 1384754856864)
